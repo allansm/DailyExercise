847934d43c3c81a98c58f37e58acf0cb2b3db004
allansm <37338692+allansm@users.noreply.github.com>
2018-04-06 14:25:40 -0300

    update files

--- resources/views/exercise_history.blade.php
+++ resources/views/exercise_history.blade.php
@@ -1,15 +1,16 @@
 @extends("layouts.dash")
 @section("dash-title","Historico diario")
 @section("dash-content")
-<div class="row table-responsive">
-    Hoje :&nbsp;<div style="background:#007bff;height:20px;width:20px;float:right;border-radius:100%"></div><br/><br/>
+<div style="float:left">Hoje :&nbsp;</div><div style="background:#007bff;height:20px;width:20px;float:left;border-radius:100%"></div><br/><br/>
+<div class=" table-responsive">
+    
     <table class="table table-sm table-dark table-striped " style="text-align: center;">
         <tr>
             <td>#</td>
             <td>Exercicio</td>
             <td>Contagem</td>
             <td>Intensidade</td>
-            <td>Data</td>
+            <td>Dia/Data/Hora</td>
             <td>Deletar</td>
         </tr>
         @foreach(Auth()->user()->account->exercise->all() as $exercise)
@@ -19,7 +20,7 @@
                     <td>{{$exercise->title}}</td>
                     <td>{{($exercise->type == "count")?"Repetição":"Tempo"}}</td>
                     <td>{{$historic->intensity}}</td>
-                    <td>{{$days[date("N",strtotime($historic->created_at))].date(" d/m/Y",strtotime($historic->created_at))}}</td>
+                    <td>{{$days[date("N",strtotime($historic->created_at))].date(" d/m/Y h:i:s",strtotime($historic->created_at))}}</td>
                     <td><a href="{{route("dashboard.deletar.historico")}}?q={{$historic->id}}" style="color:#f00">x</a></td>
                 </tr>
             @endforeach
