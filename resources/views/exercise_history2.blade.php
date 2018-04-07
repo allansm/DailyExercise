7074fb74f42617f3fb6aca7de219286b6ebe9730
allansm <37338692+allansm@users.noreply.github.com>
2018-04-06 22:14:34 -0300

    update files

--- resources/views/exercise_history2.blade.php
+++ resources/views/exercise_history2.blade.php
@@ -5,10 +5,11 @@
     @foreach(Auth()->user()->account->exercise->all() as $exercise)
         @foreach($exercise->historic->all() as $historic)
             <div class="col-lg-4 col-md-6 "  style="padding:10px ;">
+                <a style="background:#222;color:#eee" class="col-md-12" href="{{route("dashboard.deletar.historico")}}?q={{$historic->id}}">X</a>
                 <div class="col-md-12 tile tile-register transaction-1">
                     <div class="col-md-12" style="text-align:center;padding:10px;"><h3>{{$exercise->title}}</h3></div>
                     <div style="text-align: center;">
-                        <h2>{{$historic->intensity}}<br/>{{($exercise->type == "count")?"Reps":"Minutos"}}</h2><br/><h4>{{(explode(" ",$historic->created_at)[0] == date("Y-m-d"))?"Hoje":strtoupper($days[date("N",strtotime($historic->created_at))])}}<br/>{{date(" d/m/Y",strtotime($historic->created_at))}}</h4>
+                        <h2>{{$historic->intensity}}<br/>{{($exercise->type == "count")?"Repetições":"Minutos"}}</h2><br/><h4>{{(explode(" ",$historic->created_at)[0] == date("Y-m-d"))?"Hoje":strtoupper($days[date("N",strtotime($historic->created_at))])}}<br/>{{date(" d/m/Y",strtotime($historic->created_at))}}</h4>
                     </div>
                 </div>
             </div>
